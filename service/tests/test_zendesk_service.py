b1bd6e7455ea5abf4257d056ebdffea4c22d46f9
Morten Frellumstad <morten.frellumstad@sesam.io>
2019-12-16 12:41:13 +0100

    Develop tag for image and update of tests

--- service/tests/test_zendesk_service.py
+++ service/tests/test_zendesk_service.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def test_new_ticket():
     """
@@ -8,5 +9,28 @@ def test_new_ticket():
     """
     payload = [{"ticket": {"subject": "My printer is on fire!", "comment": { "body": "The smoke is very colorful." }}}]
     rv = requests.post('http://zendesk-service:5000/transform/ticket/new',json=payload)
-    assert isinstance(rv.json(),list)
+    echo = rv.json()
+    assert isinstance(echo,list)
+    echo = echo[0]
+    echo_payload = echo['payload']
+    assert isinstance(echo_payload,dict)
+    assert json.dumps([echo_payload]) == payload
+    assert echo['content_type'] == 'application/json'
+
+def test_update_ticket():
+    """
+    GIVEN a response
+    WHEN endpoint /transform/ticket/update get a call
+    THEN check response value relativ to call method and payload
+    """
+    payload = [{"ticket": {"id": "test_id","subject": "My printer is on fire!", "comment": { "body": "The smoke is very colorful." }}}]
+    rv = requests.post('http://zendesk-service:5000/transform/ticket/update',json=payload)
+    echo = rv.json()
+    assert isinstance(echo,list)
+    echo = echo[0]
+    echo_payload = echo['payload']
+    assert isinstance(echo_payload,dict)
+    assert json.dumps([echo_payload]) == payload
+    assert echo['content_type'] == 'application/json'
+
 
